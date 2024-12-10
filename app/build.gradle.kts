925d8955e8ff747fb94d87558276fba99ad94b23
Yaroslav Ognevoy <ya.ognevoy@gmail.com>
2024-12-10 02:49:24 -0200

    Add work with Word and PDF dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.wordtopdfconverter"
-        minSdk = 21
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -56,4 +56,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Work with Word and PDF
+    implementation(libs.poi)
+    implementation(libs.poi.ooxml)
+    implementation(libs.itext7.core)
 }
